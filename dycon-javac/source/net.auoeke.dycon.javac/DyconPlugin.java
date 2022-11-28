15b28b4e337d6eba2b20fbc0edb4015d51a46a72
Mohamed Maaliki <me@auoeke.net>
2022-11-28 05:25:01 +0000

    javac 0.0.4: more errors; fix initialization.
    - Delay `invokeHandle`'s initialization to an appropriate time.
    - Make each dynamic constant's name unique to avoid deduplication.
    - Verify that the argument is a functional expression.
    - Remove unnecessary dependency on `:`.
    - Publish fat JAR.

--- dycon-javac/source/net.auoeke.dycon.javac/DyconPlugin.java
+++ dycon-javac/source/net.auoeke.dycon.javac/DyconPlugin.java
@@ -1,6 +1,7 @@
 package net.auoeke.dycon.javac;
 
-import javax.lang.model.element.Name;
+import java.util.HashMap;
+import java.util.Map;
 import javax.tools.Diagnostic;
 import com.sun.source.util.JavacTask;
 import com.sun.source.util.Plugin;
@@ -11,22 +12,30 @@ import com.sun.tools.javac.api.BasicJavacTask;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Symtab;
 import com.sun.tools.javac.jvm.PoolConstant;
+import com.sun.tools.javac.model.JavacElements;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.tree.TreeTranslator;
 import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Name;
 import com.sun.tools.javac.util.Names;
 import net.auoeke.reflect.Modules;
 
 public class DyconPlugin implements Plugin, TaskListener {
+	// Contains a unique name for each ldc call in a compilation unit in order to prevent deduplication.
+	private final Map<Integer, Name> condyNames = new HashMap<>();
 	private Context context;
+	private JavacElements elements;
 	private Trees trees;
-
-	private Symbol.MethodHandleSymbol invokeHandle;
+	private TreeMaker factory;
+	private Symtab symtab;
+	private Names names;
 
 	private Name net_auoeke_dycon;
 	private Name Dycon;
 	private Name ldc;
+
+	private Symbol.MethodHandleSymbol invokeHandle;
 
 	@Override public String getName() {
 		return "dycon";
@@ -34,20 +43,14 @@ public class DyconPlugin implements Plugin, TaskListener {
 
 	@Override public void init(JavacTask task, String... args) {
 		this.context = ((BasicJavacTask) task).getContext();
+		this.elements = JavacElements.instance(this.context);
 		this.trees = Trees.instance(task);
-		var symtab = Symtab.instance(this.context);
-		var names = Names.instance(this.context);
-
-		this.invokeHandle = new Symbol.MethodHandleSymbol(symtab.enterClass(symtab.java_base, names.fromString("java.lang.invoke.ConstantBootstraps")).members().findFirst(
-			names.fromString("invoke"),
-			symbol -> symbol instanceof Symbol.MethodSymbol invoke
-				&& invoke.params.size() == 5
-				&& invoke.params.get(3).type.tsym == symtab.methodHandleType.tsym
-		));
-
-		this.net_auoeke_dycon = names.fromString("net.auoeke.dycon");
-		this.Dycon = names.fromString("Dycon");
-		this.ldc = names.fromString("ldc");
+		this.factory = TreeMaker.instance(this.context);
+		this.symtab = Symtab.instance(this.context);
+		this.names = Names.instance(this.context);
+		this.net_auoeke_dycon = this.names.fromString("net.auoeke.dycon");
+		this.Dycon = this.names.fromString("Dycon");
+		this.ldc = this.names.fromString("ldc");
 
 		task.addTaskListener(this);
 	}
@@ -59,32 +62,45 @@ public class DyconPlugin implements Plugin, TaskListener {
 	@Override public void started(TaskEvent event) {
 		if (event.getKind() == TaskEvent.Kind.GENERATE) {
 			var cu = (JCTree.JCCompilationUnit) event.getCompilationUnit();
-			var factory = TreeMaker.instance(this.context);
 
 			new TreeTranslator() {
+				int index = 0;
+
 				@Override public void visitApply(JCTree.JCMethodInvocation node) {
 					super.visitApply(node);
 
-					if (node.getMethodSelect() instanceof JCTree.JCFieldAccess field
+					if (node.meth instanceof JCTree.JCFieldAccess field
 						&& field.sym instanceof Symbol.MethodSymbol method
 						&& method.owner.packge().fullname.equals(DyconPlugin.this.net_auoeke_dycon)
 						&& method.owner.name.equals(DyconPlugin.this.Dycon)
 						&& method.name.equals(DyconPlugin.this.ldc)
 					) {
-						var metafactory = (JCTree.JCMethodInvocation) node.getArguments().get(0);
-						var initializer = (Symbol.MethodHandleSymbol) ((Symbol.DynamicMethodSymbol) ((JCTree.JCFieldAccess) metafactory.getMethodSelect()).sym).staticArgs[1];
-						var type = initializer.type.asMethodType();
+						if (node.args.head instanceof JCTree.JCMethodInvocation lmfCall && lmfCall.meth instanceof JCTree.JCFieldAccess lmf && lmf.sym instanceof Symbol.DynamicMethodSymbol bsm) {
+							var initializer = (Symbol.MethodHandleSymbol) bsm.staticArgs[1];
+							var type = initializer.type.asMethodType();
 
-						if (type.getParameterTypes().size() > 0 || !initializer.isStatic()) {
-							DyconPlugin.this.trees.printMessage(Diagnostic.Kind.ERROR, "capturing or non-static lambda cannot be intrinsified", node, cu);
+							if (type.getParameterTypes().size() > 0 || !initializer.isStatic()) {
+								DyconPlugin.this.trees.printMessage(Diagnostic.Kind.ERROR, "capturing or non-static lambda cannot be intrinsified", node.args.head, cu);
+							} else {
+								if (DyconPlugin.this.invokeHandle == null) {
+									DyconPlugin.this.invokeHandle = new Symbol.MethodHandleSymbol(DyconPlugin.this.elements.getTypeElement("java.lang.invoke.ConstantBootstraps").members().findFirst(
+										DyconPlugin.this.names.fromString("invoke"),
+										symbol -> symbol instanceof Symbol.MethodSymbol invoke
+											&& invoke.params.size() == 5
+											&& invoke.params.get(3).type.tsym == DyconPlugin.this.symtab.methodHandleType.tsym
+									));
+								}
+
+								this.result = DyconPlugin.this.factory.Ident(new Symbol.DynamicVarSymbol(
+									DyconPlugin.this.condyNames.computeIfAbsent(this.index++, index -> DyconPlugin.this.names.fromString(index.toString())),
+									null,
+									DyconPlugin.this.invokeHandle,
+									type.getReturnType(),
+									new PoolConstant.LoadableConstant[]{initializer}
+								));
+							}
 						} else {
-							this.result = factory.Ident(new Symbol.DynamicVarSymbol(
-								(com.sun.tools.javac.util.Name) DyconPlugin.this.ldc,
-								null,
-								DyconPlugin.this.invokeHandle,
-								type.getReturnType(),
-								new PoolConstant.LoadableConstant[]{initializer}
-							));
+							DyconPlugin.this.trees.printMessage(Diagnostic.Kind.ERROR, "ldc argument is not a functional expression", node.args.head, cu);
 						}
 					}
 				}
